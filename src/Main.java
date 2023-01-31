42c21d2151eeab8f082ca8edc91ff529c91dd3cf
dimab <dima.bogdashkin@gmail.com>
2023-01-31 11:45:58 +0300

    Homework done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,41 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        tusk1();
+        tusk2();
+        tusk3();
+    }
+
+    public static void tusk1() {
+        System.out.println("Задание 1");
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("ФИО сотрудника — " + fullName);
+
+        System.out.println();
+    }
+
+    public static void tusk2() {
+        System.out.println("Задание 2");
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullName.toUpperCase());
+
+        System.out.println();
+    }
+
+    public static void tusk3() {
+        System.out.println("Задание 3");
+        String firstName = "Семён";
+        String middleName = "Семёнович";
+        String lastName = "Иванов";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        fullName = fullName.replace("ё", "е");
+        System.out.println("Данные ФИО сотрудника — " + fullName);
+
+        System.out.println();
     }
 }
